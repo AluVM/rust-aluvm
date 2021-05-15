62d0a9c4ddbb83653f08e35af5b1eb42465ce42b
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-05-15 20:15:11 +0200

    -Completing instruction arguments

--- src/lib.rs
+++ src/lib.rs
@@ -34,20 +34,20 @@ pub enum ControlFlowOp {
     Succ,
 
     /// Compares value of two arithmetic (`A`) registers putting result into `cm0`
-    #[value = 0b110] // 3 + 4 + 4 => 11 bits
-    Cmpa(RegA, Reg16, Reg16),
+    #[value = 0b110] // 3 + 5 + 3 + 5 => 16 bits
+    Cmpa(RegA, Reg32, RegA, Reg32),
 
     /// Compares value of two non-arithmetic (`R`) registers putting result into `cm0`
-    #[value = 0b111] // 2 * 4 * 4 = 5 bits
-    Cmpr(RegR, Reg4, Reg4),
+    #[value = 0b111]
+    Cmpr(RegR, Reg32, RegR, Reg32),
 
     /// Checks equality of value in two arithmetic (`A`) registers putting result into `st0`
     #[value = 0b100]
-    Eqa(RegA, Reg16, Reg16),
+    Eqa(RegA, Reg32, RegA, Reg32),
 
     /// Checks equality of value in two non-arithmetic (`R`) registers putting result into `st0`
     #[value = 0b101]
-    Eqr(RegR, Reg4, Reg4),
+    Eqr(RegR, Reg32, RegR, Reg32),
 
     /// Unconditionally jumps to an offset. Increments `cy0`.
     #[value = 0b010]
@@ -62,71 +62,89 @@ pub enum RegisterOp {
     Split,
     Join,
     /// Puts a value into a register
-    Put(RegA, Reg16),
+    Put(RegA, Reg32),
+
     Mova,
-    Movap,
     Movr,
+
+    Swpa,
+    Swpr,
 
     /// Cleans register value
     Cln,
 }
 
 pub enum ArithmeticOp {
-    Neg(RegA, Reg16, bool), // 3 + 4 + 1 = 8 bits
-    Add(Arithmetics, RegA, Reg16, Reg16), // 3 + 3 + 4 + 4  => 14 bits
-    Sub(Arithmetics, RegA, Reg16, Reg16),
-    Mul(Arithmetics, RegA, Reg16, Reg16),
-    Div(Arithmetics, RegA, Reg16, Reg16),
-    Mod(RegA, Reg16, bool /** Put the result into `A1` */), // 3 + 4 + 1 = 8 bits
-    Abs(RegA, Reg16, RegA, Reg16), // 3 + 4 + 3 + 4 => 14 bits
+    Neg(RegA, Reg32), // 3 + 5 = 8 bits
+    Add(Arithmetics, RegA, Reg32, Reg32), // 3 + 3 + 5 + 5  => 16 bits
+    Sub(Arithmetics, RegA, Reg32, Reg32),
+    Mul(Arithmetics, RegA, Reg32, Reg32),
+    Div(Arithmetics, RegA, Reg32, Reg32),
+    Mod(RegA, Reg32), // 3 + 5 = 8 bits
+    Abs(RegA, Reg32, RegA, Reg32), // 3 + 5 + 3 + 5 => 16 bits
 }
 
 pub enum BitwiseOp {
-    And,
-    Or,
-    Xor,
-    Not,
-    Shl,
-    Shr,
+    And(RegA, Reg32, Reg32, Reg8 /** Operation destination, only first 8 registers */),
+    Or(RegA, Reg32, Reg32, Reg8),
+    Xor(RegA, Reg32, Reg32, Reg8),
 
+    Not(RegA, Reg32),
+
+    Shl(RegA, Reg32, Reg32 /** Always `a8` */, Reg8),
+    Shr(RegA, Reg32, Reg32, Reg8),
     /// Shift-cycle left
-    Scl,
-
+    Scl(RegA, Reg32, Reg32, Reg8),
     /// Shift-cycle right
-    Scr,
+    Scr(RegA, Reg32, Reg32, Reg8),
 }
 
 #[non_exhaustive]
 pub enum DigestOp {
-    Ripemd,
-    Sha2,
-    Sha3
+    Ripemd(
+        Reg32 /** Which of `a16` registers contain start offset */,
+        Reg32 /** Index of string register */,
+        Reg32 /** Index of `r160` register to save result to */,
+        bool /** Clear string register after operation */
+    ),
+    Sha2(
+        Reg32 /** Which of `a16` registers contain start offset */,
+        Reg32 /** Index of string register */,
+        Reg32 /** Index of `r160` register to save result to */,
+        bool /** Clear string register after operation */
+    ),
 }
 
 pub enum SecpOp {
-    Gen,
-    Add,
-    Neg,
+    Gen(Reg8),
+    Mul(
+        bool, /** Use `a` or `r` register as scalar source */
+        Reg32, /** Scalar register index */
+        Reg32, /** Source `r` register index containing EC point */
+        Reg32, /** Destination `r` register index */
+    ),
+    Add(
+        bool, /** Allow overflows */
+        Reg32, /** Source 1 */
+        Reg32, /** Source 2 */
+        Reg32, /** Source 3 */
+    ),
+    Neg(
+        Reg32,
+        Reg8,
+    ),
 }
 
 pub enum Ed25519Op {
     Gen,
+    Mul,
     Add,
     Neg,
 }
 
 #[derive(Debug, Display)]
 #[display(Debug)]
-pub enum Reg4 {
-    Reg1,
-    Reg2,
-    Reg3,
-    Reg4,
-}
-
-#[derive(Debug, Display)]
-#[display(Debug)]
-pub enum Reg16 {
+pub enum Reg32 {
     Reg1,
     Reg2,
     Reg3,
@@ -143,6 +161,22 @@ pub enum Reg16 {
     Reg14,
     Reg15,
     Reg16,
+    Reg17,
+    Reg18,
+    Reg19,
+    Reg20,
+    Reg21,
+    Reg22,
+    Reg23,
+    Reg24,
+    Reg25,
+    Reg26,
+    Reg27,
+    Reg28,
+    Reg29,
+    Reg30,
+    Reg31,
+    Reg32,
 }
 
 #[derive(Debug, Display)]
@@ -161,10 +195,14 @@ pub enum RegA {
 #[derive(Debug, Display)]
 #[display(Debug)]
 pub enum RegR {
+    R128,
     R160,
     R256,
     R512,
     R1024,
+    R2048,
+    R4096,
+    R8192,
 }
 
 pub enum Arithmetics {
@@ -178,22 +216,29 @@ pub enum Arithmetics {
 #[derive(Debug)]
 struct Registers {
     // Arithmetic registers:
-    a8: [Option<u8>; 16],
-    a16: [Option<u16>; 16],
-    a32: [Option<u32>; 16],
-    a64: [Option<u64>; 16],
-    a128: [Option<u128>; 16],
-    a256: [Option<u256>; 16],
-    a512: [Option<u512>; 16],
+    a8: [Option<u8>; 32],
+    a16: [Option<u16>; 32],
+    a32: [Option<u32>; 32],
+    a64: [Option<u64>; 32],
+    a128: [Option<u128>; 32],
+    a256: [Option<u256>; 32],
+    a512: [Option<u512>; 32],
 
     /// Arbitrary-precision arithmetics registers
-    ap: [Option<Box<[u8]>>; 16],
+    ap: [Option<Box<[u8]>>; 32],
 
     // Non-arithmetic registers:
-    r160: [Option<[u8; 20]>; 4],
-    r256: [Option<[u8; 32]>; 4],
-    r512: [Option<[u8; 64]>; 4],
-    r4096: [Option<[u8; 512]>; 4],
+    r128: [Option<[u8; 16]>; 32],
+    r160: [Option<[u8; 20]>; 32],
+    r256: [Option<[u8; 32]>; 32],
+    r512: [Option<[u8; 64]>; 32],
+    r1024: [Option<[u8; 128]>; 32],
+    r2048: [Option<[u8; 256]>; 32],
+    r4096: [Option<[u8; 512]>; 32],
+    r8192: [Option<[u8; 1024]>; 32],
+
+    /// String and bytestring registers
+    s16: [Option<[u8; u16::MAX as usize]>; 32],
 
     /// Control flow register which stores result of comparison operations. Initialized with `0`
     cm0: Ordering,
