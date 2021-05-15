2cab4a84b5e8bcaf8935925af1e0781d45f58295
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-05-15 14:32:19 +0200

    Draft instruction set

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,69 @@
 
 
+#[non_exhaustive]
+enum Instruction {
+    ControlFlow(ControlFlowOp),
+    Register(RegisterOp),
+    Arithmetic(ArithmeticOp),
+    Bitwise(BitwiseOp),
+    Digest(DigestOp),
+    Secp256k1(SecpOp),
+    Ed25519(Ed25519Op),
+    // Other curves may be added later
+}
+
+enum ControlFlowOp {
+    Succ,
+    Fail,
+    Cmp,
+    Jmp,
+    Jif,
+}
+
+enum RegisterOp {
+    Split,
+    Join,
+    Mov,
+}
+
+enum ArithmeticOp {
+    Neg,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Abs,
+}
+
+enum BitwiseOp {
+    And,
+    Or,
+    Xor,
+    Not,
+    Shl,
+    Shr,
+}
+
+#[non_exhaustive]
+enum DigestOp {
+    Ripemd,
+    Sha2,
+    Sha3
+}
+
+enum SecpOp {
+    Gen,
+    Add,
+    Neg,
+}
+
+enum Ed25519Op {
+    Gen,
+    Add,
+    Neg,
+}
+
 #[derive(Debug)]
 struct Registers {
     // Arithmetic registers:
@@ -9,7 +73,8 @@ struct Registers {
     a32: [Option<u32>; 16],
     a64: [Option<u64>; 16],
     a128: [Option<u128>; 16],
-    a256: [Option<Uint256>; 16],
+    a256: [Option<u256>; 16],
+    a512: [Option<u512>; 16],
 
     // Arbitrary-precision
     aap: [Option<Box<[u8]>>; 16],
@@ -22,4 +87,10 @@ struct Registers {
 
     // Control flow registers
     cf0: u16,
+}
+
+impl Registers {
+    pub fn execute(&mut self, code: &[u8]) {
+
+    }
 }
