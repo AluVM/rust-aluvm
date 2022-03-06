5e0900201d161c6d0e2b3dde2c4913e481f870e0
z8674558 <own7000hr@gmail.com>
2022-03-06 21:00:24 +0900

    support bitwise on Number

--- src/data/bitwise.rs
+++ src/data/bitwise.rs
@@ -12,7 +12,9 @@
 use core::convert::TryFrom;
 use core::ops::{BitAnd, BitOr, BitXor, Not, Shl, Shr};
 
-use super::{MaybeNumber, Number, NumberLayout};
+use amplify::num::{i1024, u1024};
+
+use super::{MaybeNumber, Number};
 
 impl Not for MaybeNumber {
     type Output = MaybeNumber;
@@ -65,11 +67,19 @@ impl Shl for Number {
 
     #[inline]
     fn shl(self, rhs: Self) -> Self::Output {
-        assert!(self.layout().is_integer(), "bit shifting float number");
-        self.to_u1024_bytes()
-            .shl(u16::try_from(rhs).expect("attempt to bitshift left for more than 2^16 bits")
-                as usize)
-            .into()
+        let layout = self.layout();
+        assert!(layout.is_integer(), "bit shifting float number");
+        let rhs = u16::try_from(rhs).expect("attempt to bitshift lhs for more than 2^16 bits");
+        let mut n = match layout.is_signed_int() {
+            true => {
+                Number::from(self.to_i1024_bytes().checked_shl(rhs as u32).unwrap_or(i1024::ZERO))
+            }
+            false => {
+                Number::from(self.to_u1024_bytes().checked_shl(rhs as u32).unwrap_or(u1024::ZERO))
+            }
+        };
+        n.reshape(layout);
+        n
     }
 }
 
@@ -78,46 +88,47 @@ impl Shr for Number {
 
     #[inline]
     fn shr(self, rhs: Self) -> Self::Output {
-        assert!(self.layout().is_integer(), "bit shifting float number");
-        self.to_u1024_bytes()
-            .shr(u16::try_from(rhs).expect("attempt to bitshift right for more than 2^16 bits")
-                as usize)
-            .into()
+        let layout = self.layout();
+        assert!(layout.is_integer(), "bit shifting float number");
+        let rhs = u16::try_from(rhs).expect("attempt to bitshift right for more than 2^16 bits");
+        let mut n = match layout.is_signed_int() {
+            true => {
+                Number::from(self.to_i1024_bytes().checked_shr(rhs as u32).unwrap_or(i1024::ZERO))
+            }
+            false => {
+                Number::from(self.to_u1024_bytes().checked_shr(rhs as u32).unwrap_or(u1024::ZERO))
+            }
+        };
+        n.reshape(layout);
+        n
     }
 }
 
 impl Number {
     /// Cyclic bit shift left. Panics if the number is not an integer.
     pub fn scl(self, shift: Number) -> Number {
-        assert!(self.layout().is_integer(), "bit shifting float number");
-        let excess = u16::try_from(shift).expect(
+        let layout = self.layout();
+        let bits = self.len() * 8;
+        let lhs = self.into_unsigned();
+        assert!(layout.is_integer(), "bit shifting float number");
+        let excess = u16::try_from(shift).map(|v| v % bits).expect(
             "shift value in `scl` operation must always be from either `a8` or `a16` registry",
         );
-        let residue = self >> Number::from(self.len() - excess);
-        (self << shift) | residue
+        let residue = lhs >> Number::from(bits - excess);
+        ((lhs << Number::from(excess)) | residue).reshaped(layout, true).expect("restoring layout")
     }
 
     /// Cyclic bit shift right. Panics if the number is not an integer.
     pub fn scr(self, shift: Number) -> Number {
-        assert!(self.layout().is_integer(), "bit shifting float number");
-        let excess = u16::try_from(shift).expect(
+        let layout = self.layout();
+        let bits = self.len() * 8;
+        let lhs = self.into_unsigned();
+        assert!(layout.is_integer(), "bit shifting float number");
+        let excess = u16::try_from(shift).map(|v| v % bits).expect(
             "shift value in `scl` operation must always be from either `a8` or `a16` registry",
         );
-        let residue = self << Number::from(self.len() - excess);
-        (self >> shift) | residue
-    }
-
-    /// Bit shift right for signed value. Panics if the number is not an integer.
-    pub fn shr_signed(self, shift: Number) -> Number {
-        assert!(self.layout().is_integer(), "bit shifting float number");
-        if self.layout().bits() > 128 {
-            todo!("(#15) implement signed right bit shift")
-        }
-        let shift = u16::try_from(shift).expect(
-            "shift value in `scl` operation must always be from either `a8` or `a16` registry",
-        );
-        let val = i128::from(self);
-        Number::from(val << shift)
+        let residue = lhs << Number::from(bits - excess);
+        ((lhs >> Number::from(excess)) | residue).reshaped(layout, true).expect("restoring layout")
     }
 
     /// Reverses the order of bits in the integer. The least significant bit becomes the most
@@ -128,5 +139,60 @@ impl Number {
         bytes.reverse();
         bytes.iter_mut().for_each(|byte| *byte = byte.reverse_bits());
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn shl_test() {
+        let x = Number::from(6u8);
+        let y = Number::from(24u8);
+        assert_eq!(x.shl(Number::from(2)), y);
+        let x = Number::from(-1i16);
+        let y = Number::from(-2i16);
+        assert_eq!(x.shl(Number::from(1)), y);
+    }
+
+    #[test]
+    fn shr_test() {
+        let x = Number::from(9u8);
+        let y = Number::from(4u8);
+        assert_eq!(x.shr(Number::from(1)), y);
+        let x = Number::from(-2i16);
+        let y = Number::from(-1i16);
+        assert_eq!(x.shr(Number::from(1)), y);
+    }
+
+    #[test]
+    fn scl_test() {
+        let x = Number::from(131u8);
+        let y = Number::from(7u8);
+        assert_eq!(x.scl(Number::from(1)), y);
+        let x = Number::from(-7i16);
+        let y = Number::from(-25i16);
+        assert_eq!(x.scl(Number::from(2)), y);
+    }
+
+    #[test]
+    fn scr_test() {
+        let x = Number::from(129u8);
+        let y = Number::from(192u8);
+        assert_eq!(x.scr(Number::from(1)), y);
+        let x = Number::from(1i8);
+        let y = Number::from(64i8);
+        assert_eq!(x.scr(Number::from(2)), y);
+    }
+
+    #[test]
+    fn reverse_bits_test() {
+        let x = Number::from(192u8);
+        let y = Number::from(3u8);
+        assert_eq!(x.reverse_bits(), y);
+        let x = Number::from(1i8);
+        let y = Number::from(-128i8);
+        assert_eq!(x.reverse_bits(), y);
     }
 }
