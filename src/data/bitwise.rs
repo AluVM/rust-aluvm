57eae5172bab95928de745693cc47b59ca2ef8c7
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-12 17:39:15 +0200

    Update license header

--- src/data/bitwise.rs
+++ src/data/bitwise.rs
@@ -1,8 +1,8 @@
 // Reference rust implementation of AluVM (arithmetic logic unit virtual machine).
 // To find more on AluVM please check <https://github.com/internet2-org/aluvm-spec>
 //
-// Designed & written in 2021 by
-//     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
+// Designed & written in 2021-2022 by
+//     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
 // This work is donated to LNP/BP Standards Association by Pandora Core AG
 //
 // This software is licensed under the terms of MIT License.
