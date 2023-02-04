2bde05b47e10fc0a06eb12c868a14dbeff1551ec
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2023-02-04 13:56:45 +0100

    chore: housekeeping

--- src/reg/core_regs.rs
+++ src/reg/core_regs.rs
@@ -1,13 +1,25 @@
 // Reference rust implementation of AluVM (arithmetic logic unit virtual machine).
-// To find more on AluVM please check <https://github.com/internet2-org/aluvm-spec>
+// To find more on AluVM please check <https://aluvm.org>
 //
-// Designed & written in 2021-2022 by
-//     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
-// This work is donated to LNP/BP Standards Association by Pandora Core AG
+// SPDX-License-Identifier: Apache-2.0
 //
-// This software is licensed under the terms of MIT License.
-// You should have received a copy of the MIT License along with this software.
-// If not, see <https://opensource.org/licenses/MIT>.
+// Written in 2021-2023 by
+//     Dr Maxim Orlovsky <orlovsky@ubideco.org>
+//
+// Copyright (C) 2021-2022 LNP/BP Standards Association. All rights reserved.
+// Copyright (C) 2023 UBIDECO Institute. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use alloc::boxed::Box;
 use alloc::string::ToString;
