187f666182a86a429a44d956246f3392dcc637ee
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2023-03-04 23:47:10 +0100

    stl: set up library generation

---
+++ scripts/typelib.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cargo run --features stl aluvm-stl -s
+cargo run --features stl aluvm-stl -b
+cargo run --features stl aluvm-stl -h
---
+++ src/bin/aluvm-stl.rs
@@ -0,0 +1,89 @@
+// Reference rust implementation of AluVM (arithmetic logic unit virtual machine).
+// To find more on AluVM please check <https://aluvm.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Written in 2021-2023 by
+//     Dr Maxim Orlovsky <orlovsky@ubideco.org>
+//
+// Copyright (C) 2021-2022 LNP/BP Standards Association. All rights reserved.
+// Copyright (C) 2023 UBIDECO Institute. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[macro_use]
+extern crate amplify;
+#[macro_use]
+extern crate strict_types;
+
+use std::io::stdout;
+use std::str::FromStr;
+use std::{env, fs, io};
+
+use aluvm::library::LibSite;
+use aluvm::LIB_NAME_ALUVM;
+use amplify::num::u24;
+use strict_encoding::{StrictEncode, StrictWriter, STRICT_TYPES_LIB};
+use strict_types::typelib::LibBuilder;
+use strict_types::{Dependency, TypeLibId};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let sty_id =
+        TypeLibId::from_str("subject_leonid_rudolf_3VG9Cjoyx9MMAY2y4EZBgX9YQoyMngFfNrGLAUFbzQFU")?;
+
+    let imports = bset! {
+        Dependency::with(sty_id, libname!(STRICT_TYPES_LIB)),
+    };
+
+    let lib = LibBuilder::new(libname!(LIB_NAME_ALUVM)).process::<LibSite>()?.compile(imports)?;
+    let id = lib.id();
+
+    let ext = match args.get(2).map(String::as_str) {
+        Some("-b") => "stl",
+        Some("-h") => "asc.stl",
+        _ => "sty",
+    };
+    let filename = args.get(3).cloned().unwrap_or_else(|| format!("stl/AluVM.{ext}"));
+    let mut file = match args.len() {
+        2 => Box::new(stdout()) as Box<dyn io::Write>,
+        3 | 4 => Box::new(fs::File::create(filename)?) as Box<dyn io::Write>,
+        _ => panic!("invalid argument count"),
+    };
+    match ext {
+        "stl" => {
+            lib.strict_encode(StrictWriter::with(u24::MAX.into_usize(), file))?;
+        }
+        "asc.stl" => {
+            writeln!(file, "{lib:X}")?;
+        }
+        _ => {
+            writeln!(
+                file,
+                "{{-
+  Id: {id}
+  Name: AluVM
+  Description: Consensus layer for RGB smart contracts
+  Author: Dr Maxim Orlovsky <orlovsky@ubideco.org>
+  Copyright (C) 2023 UBIDECO Institute. All rights reserved.
+  License: Apache-2.0
+-}}
+"
+            )?;
+            writeln!(file, "{lib}")?;
+        }
+    }
+
+    Ok(())
+}
